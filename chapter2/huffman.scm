7a2841a50f7b8b61e71c4537084ef3a47c50ff45
huhuang03 <huhuang03@gmail.com>
2016-12-09 11:30:15 +0800

    auto commit

---
+++ chapter2/huffman.scm
@@ -0,0 +1,41 @@
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+
+(define (symbol-leaf x) (cadr x))
+(define (weight-leaf x) (caddr x))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(define (decode bits tree)
+  (define (decode-l bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch
+               (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-l (cdr bits) tree))
+              (decode-l (cdr bits) (next-branch))))))
+  (decode-l bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch)
+        ((= bit 1) (right-banch branch))
+        (else (error "bad bit -- CHOOSE-BRANCH" bit)))))
