9727e8bb84d59b3b6e38682c05c213dade6b0204
huhuang03 <huhuang03@gmail.com>
2016-12-25 14:19:06 +0800

    auto commit

--- chapter2/huffman.scm
+++ chapter2/huffman.scm
@@ -68,4 +68,6 @@
                                    (make-leaf 'C 1)))))
 (define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
 
-(display (decode sample-message sample-tree))
+(display sample-tree)
+
+;; (display (decode sample-message sample-tree))
