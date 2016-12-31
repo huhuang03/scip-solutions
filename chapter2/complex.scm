fa5a354ecdc4d0db6c42d182871bf71b472ffb87
huhuang03 <huhuang03@gmail.com>
2016-12-31 14:44:33 +0800

    auto commit

--- chapter2/complex.scm
+++ chapter2/complex.scm
@@ -43,3 +43,21 @@
          (angle-polar (contents z)))
         (else (error "Unkown type -- ANGLE" z))))
 
+(define (attach-tag type-tag contents)
+  （cons (type-tag contents)))
+
+(define (type-tag datum)
+  (if (pair? datum)
+      (car datum)
+      (error "Bad tagged datum -- TYPE-TAG" datum)))
+
+(define (contents datum)
+  (if (pair? datum)
+      (cdr datum)
+      (error "Bad tagged datum -- CONTENTS" datum)))
+
+(define (rectangular? z)
+  (eq? (type-tag z) 'rectangular))
+
+(define (polar? z)
+  (eq? (type-tag z) 'polar))
