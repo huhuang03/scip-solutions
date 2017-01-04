92c67e817df77fa9292b68fd87881914272e739c
huhuang03 <huhuang03@gmail.com>
2017-01-04 11:30:16 +0800

    auto commit

--- chapter2/generic.scm
+++ chapter2/generic.scm
@@ -29,3 +29,28 @@
                          (+ (imag-part z1) (imag-part z2))))
   put 'add (complex complex)
   (lambda (z1 z2) (tag add-complex z1 z2)))
+
+(define (scheme-number->complex n)
+  (make-complex-from-real-imag (conents n) 0))
+
+(put-coercion 'scheme-number 'complex scheme-number->complex)
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (if (= (length args) 2)
+              (let ((type1 (car type-tags))
+                    (type2 (cadr type-tags))
+                    (a1 (car args))
+                    (a2 (cadr args)))
+                (let ((t1->t2 (get-coercion type1 type2))
+                      (t2->t1 (get-coercion type2 type1)))
+                  (cond (t1->t2
+                         (apply-generic op (t1->t2 a1) a2))
+                        (t2->t1
+                         (apply-generic op a1 (t2->t1 a2)))
+                        (else
+                         (error "No method for these types "
+                                (list op type-tags))))))))))
