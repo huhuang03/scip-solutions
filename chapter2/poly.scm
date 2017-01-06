5efaccc39cc83c4e1d1172098fd0dbb9f5449937
huhuang03 <huhuang03@gmail.com>
2017-01-06 11:30:13 +0800

    auto commit

---
+++ chapter2/poly.scm
@@ -0,0 +1,25 @@
+(Define (add-poly p1 p2)
+  (if (same-variable? (variable p1) (variable p2))
+      (make-poly (variable p1)
+                 (add-terms (term-list p1)
+                            (term-list p2)))
+      (error "Polys not in same var -- ADD_POLY"
+             (list p1 p2))))
+
+(define (mul-pol p1 p2)
+  (if (same-variable? (variable p1) (variable p2))
+      (make-poly (variable p1)
+                 (mul-terms (term-list p1)
+                            (term-list p2)))
+      (error "Polys not in same var -- MUL_POLY"
+             (list p1 p2))))
+
+(define (install-polynomial-package)
+  ;; internal procedures
+  ;; representation of poly
+  (define (make-poly variable term-list)
+    (cons variable term-list))
+  (define (variable p) (car p))
+  (define (term-list p) (cdr p))
+  (put 'add '(polynomial polynomial)
+       (lambda (p1 p2) (tag (add-poly p1 p2)))))
